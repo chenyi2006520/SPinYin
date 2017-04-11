d141108e090b5ae562188dcb60dd09fb4a95fda2
chenyi2006520 <chenyi2006520@outlook.com>
2017-04-11 22:21:38 +0800

    json词典转换

---
+++ SPinYin/SPinYin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPinYin
+{
+    public static class SPinYin
+    {
+        public static string Conver(string inputContent)
+        {
+            return null;
+        }
+    }
+}
--- SPinYinAPP/Program.cs
+++ SPinYinAPP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace SPinYinAPP
     {
         static void Main(string[] args)
         {
+            SpeechSynthesizer hello = new SpeechSynthesizer();
+            while (true)
+            {
+                string str = "请输入您的名字";
+                hello.Speak(str);  //Speak(string),Speak加上字符串类型的参数
+                string youName = Console.ReadLine();
+                if (youName == "quit")
+                {
+                    hello.Speak("拜拜。再见。");  //Speak(string),Speak加上字符串类型的参数
+                    return;
+                }
+                hello.Speak("你好" + youName);  //Speak(string),Speak加上字符串类型的参数 
+            }
         }
     }
 }
